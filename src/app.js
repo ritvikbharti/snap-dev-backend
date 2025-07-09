f55e6d3ff1cf0beb4641e19597e8cea1ccf9cdce
Ritvik Bharti <bhartiritvik1000@gmail.com>
2025-07-09 23:16:48 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -6,9 +6,23 @@ const app = express()
 
 // console.log("server is listenong");
 
-app.use((req,res)=>{
-    res.send("Hello from the server");
+// app.use('/',(req,res)=>{
+//     res.send("Hello from the server");
+// })
+app.get('/user',(req,res,next)=>{
+    console.log(req.query);
+    // res.send({firstname: "Ritvik", lastname:"Bharti"});
+    next();
+},(req,res,next)=>{
+    // res.send("2nd Respomse!");
+    next();
+},(req,res,next)=>{
+    res.send("3rd Response!");
+    
 })
 
+app.get('/hello',(req,res)=>{
+    res.send("Hello From Maa")
+})
 
 app.listen(3000)
